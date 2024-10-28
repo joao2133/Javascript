8170ca0b3dda0c8955162a1878a44ba35f5a6009
joao2133 <joaopedrochaves365@gmail.com>
2024-10-28 14:23:22 -0300

    ex pratico 002 completo

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 6 & hora < 13){
+        //BOM DIA!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#a0aeaf'
+    }else if (hora >= 12 & hora <= 18) {
+        //BOA TARDE!
+        img.src = 'fototarde.png'
+        document.body.style.background ='#bea084'
+    }else {
+        //BOA NOITE!
+        img.src = 'fotonoite.png'
+        document.body.style.background ='#0b0601'
+    }
+}
