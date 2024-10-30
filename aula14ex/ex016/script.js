06461aa1dfced52389ad1aa14c0e7cc2106df6e2
joao2133 <joaopedrochaves365@gmail.com>
2024-10-30 09:25:25 -0300

    comentarios ex anterior

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,24 +1,28 @@
 function contar(){
+    //definição de variaveis
     let ini =  document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
 
+    //Verificador de erro
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         //window.alert('[ERRO] Faltam dados!')]
         res.innerHTML = 'Impossível Contar!'
     }else{
         res.innerHTML = 'Contando: <br> '
-        let i = Number(ini.value) /* Convertendo as variaveis para numero*/
+        //Convertendo as variaveis para numero
+        let i = Number(ini.value) 
         let f = Number(fim.value)
         let p = Number(passo.value)
+        //se usuario colocar 0 passo, então passo 1 é considerado
         if (p <= 0){
             window.alert('Passo invalido! Considerando Passo 1')
             p = 1
         }
 
         // =+ na linha 19 é concatenação. Concatenação em JavaScript é a operação de unir o conteúdo de duas ou mais strings.
-
+        //Condições
         if (i < f){
             //Contagem crescente
             for (let c = i; c <=f; c += p){
