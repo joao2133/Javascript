8d85ed373447d22eb622b0bc910def7a0918257a
joao2133 <joaopedrochaves365@gmail.com>
2024-10-30 09:12:19 -0300

    comentarios

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,15 +1,20 @@
 function tabuada(){
+    //Declaração da Função
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
+    //Verificação do Campo de Entrada
     if (num.value.length == 0){
         window.alert('Por favor, digite um número!')
     }else{
+        //Conversão do Valor:
         let n = Number(num.value)
+        //limpa o conteúdo do elemento tab para remover qualquer tabuada anterior.
         tab.innerHTML = ''
+        //Laço de Repetição para a Tabuada
         for(c =1; c<=10; c++){
-            let item = document.createElement('option')
+            let item = document.createElement('option') //Um novo elemento <option> é criado
             item.text = `${n} x ${c} = ${n*c}`
-            tab.appendChild(item)
+            tab.appendChild(item)//anexa item ao final da lista de filhos de tab
         }
     }
 }
